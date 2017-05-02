9e9085d9bf841f58ea97569a29d735385deb44ef
lonce <lonce.wyse@nus.edu.sg>
2017-05-02 11:54:58 +0800

    shell scripts

--- foo.sh
+++ foo.sh
@@ -1,54 +1,17 @@
-#!/bin/bash                                                                                                                                                                      
-# To store logs and see both stderr and stdout on the screen:                                                                                                                    
-#    nohup ./run50.sh >>logs/multilog.txt 2>&1 &                                                                                                                                 
-# Individual logs will also still get stored in their respective directories                                                                                                    
-source activate tflow2
-DATE=`date +%Y.%m.%d`
-maindir=logs.$DATE
-mkdir $maindir
+#!/bin/bash                                                                                                                                                                   
 
-epsilon=1.0
-optimizer=adam
-learningrate=.01
-orientation=channels
-layers=2
-mtl=8
-l2channels=64
-fcsize=32
+contentArray=(BeingRural5.0 agf5.0 Nancarrow5.0)
+styleArray=(BeingRural5.0 agf5.0 Nancarrow5.0)
 
-l1channelsArray=(16 32)
-epsilonArray=(0.1 1.0)
-
-
-for l1channels in ${l1channelsArray[@]}
+for content in ${contentArray[@]}
 do
-    for l2channels in ${l2channelsArray[@]}
+    for style in ${styleArray[@]}
     do
-        for fcsize in ${fcsizeArray[@]}
-        do
-            #make output dir for paramter settings                                                                                                                               
-            echo " -------       new batch run     --------"
-            OUTDIR="$maindir/l1r_${l1channels}.l2_${l2channels}.fc_${fcsize}"
-            mkdir $OUTDIR
-            echo "outdir is " $OUTDIR
-
-            #keep a copy of this run file                                                                                                                                        
-            me=`basename "$0"`
-            cp $me $OUTDIR
-
-            #make subdirs for logging and checkpoints                                                                                                                            
-            mkdir "$OUTDIR/log_graph"
-            mkdir "$OUTDIR/checkpoints"
-            mkdir "$OUTDIR/stderr"
-            # wrap python call in a string so we can do our fancy redirecting below                                                                                              
-            runcmd='python DCNSoundClass.py --outdir $OUTDIR --checkpointing 1 --checkpointPeriod 1000  '
-            runcmd+='--numClasses 50 --batchsize 20 --n_epochs 200 --learning_rate ${learningrate}  '
-            runcmd+='--keepProb .5 --l1channels ${l1channels} --l2channels ${l2channels} --fcsize ${fcsize} --freqorientation ${orientation}  '
-            runcmd+='--numconvlayers ${layers} --adamepsilon ${epsilon} --optimizer ${optimizer} --mtlnumclasses ${mtl}'
-                        # direct stdout and sterr from each run into their proper directories, but tww so we can still watch                                                     
-                eval $runcmd > >(tee $OUTDIR/log.txt) 2> >(tee $OUTDIR/stderr/stderr.log >&2)
-        done
+	if [ "$style" == "$content" ]
+	then
+	   continue
+	fi
+	echo $content $style
     done
 done
-
 
--- runstyle.sh
+++ runstyle.sh
@@ -1,29 +1,21 @@
 #!/bin/bash                                                                                                                                                        
-<<<<<<< HEAD
 #    nohup ./runstyle.sh >>styleout/2017.05.02/multilog.txt 2>&1 &                                                                                                 
-=======
-#    nohup ./runstyle.sh >>styleout/2017.04.29/multilog.txt 2>&1 &                                                                                                 
->>>>>>> b89fbe6be843caa0b4bcf1957e0fe2a48fb0fa2b
 # Individual logs will also still get stored in their respective directories                                                                                       
 source activate tflow2
 DATE=`date +%Y.%m.%d`
 maindir=styleout/$DATE
 mkdir $maindir
 
-<<<<<<< HEAD
 statefile=testmodel/1d/state.pickle
-iter=4000
+iter=3000
+alpha=0
 beta=200
-noise=.4
-=======
-statefile=testmodel/state.pickle
-iter=2000
-
-noise=.2
->>>>>>> b89fbe6be843caa0b4bcf1957e0fe2a48fb0fa2b
-randArray=(0 1)
-contentArray=(BeingRural5.0 agf5.0 Superstylin5.0 roosters5.0 Nancarrow5.0 Toys5.0 inc5.0 sheepfarm5.0)
-styleArray=(BeingRural5.0 agf5.0 Superstylin5.0 roosters5.0 Nancarrow5.0 Toys5.0 inc5.0 sheepfarm5.0)
+noise=1.0
+randArray=(0)
+#contentArray=(BeingRural5.0 agf5.0 Superstylin5.0 roosters5.0 Nancarrow5.0 Toys5.0 inc5.0 sheepfarm5.0)
+#styleArray=(BeingRural5.0 agf5.0 Superstylin5.0 roosters5.0 Nancarrow5.0 Toys5.0 inc5.0 sheepfarm5.0)
+contentArray=(BeingRural5.0 agf5.0 Nancarrow5.0)
+styleArray=(BeingRural5.0 agf5.0 Nancarrow5.0)
 
 for  rand in ${randArray[@]}
 do
@@ -32,14 +24,15 @@ do
         for style in ${styleArray[@]}
         do
 
+	    if [ "$style" == "$content" ]
+	    then
+	       continue
+	    fi
            #make output dir for paramter settings                                                                                                                 \
                                                                                                                                                                    
             echo " -------       new batch run     --------"
-<<<<<<< HEAD
+
             OUTDIR="$maindir/content_${content}.style_${style}.beta_${beta}.rand_${rand}"
-=======
-            OUTDIR="$maindir/content_${content}.style_${style}.rand_${rand}"
->>>>>>> b89fbe6be843caa0b4bcf1957e0fe2a48fb0fa2b
             mkdir $OUTDIR
             echo "outdir is " $OUTDIR
 
@@ -49,12 +42,7 @@ do
             mkdir "$OUTDIR/checkpoints"
 
                         runcmd='python style_transfer.py --content ${content} --style ${style}  --noise ${noise} --outdir $OUTDIR '
-<<<<<<< HEAD
-                        runcmd+='--stateFile ${statefile} --iter $iter --alpha 10 --beta ${beta} --randomize ${rand}'
-=======
-                        runcmd+='--stateFile ${statefile} --iter $iter --alpha 10 --beta 200 --randomize ${rand}'
->>>>>>> b89fbe6be843caa0b4bcf1957e0fe2a48fb0fa2b
-
+                        runcmd+='--stateFile ${statefile} --iter $iter --alpha ${alpha} --beta ${beta} --randomize ${rand}'
                         eval $runcmd > >(tee $OUTDIR/log.txt) 2> >(tee $OUTDIR.stderr.log >&2)
         done
     done
